80ab134825bdc244f211c8c3cc414e5b7a557753
joe <2193426510@qq.com>
2024-11-21 16:43:22 +0800

    微信支付业务功能代码开发（配置文件不全）

--- sky-server/src/main/java/com/sky/service/OrderSubmitService.java
+++ sky-server/src/main/java/com/sky/service/OrderSubmitService.java
@@ -1,11 +1,31 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 
 /**
  * 订单提交
  */
 public interface OrderSubmitService {
+    /**
+     * 订单提交
+     * @param ordersSubmitDTO
+     * @return
+     */
     OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 订单支付
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
 }

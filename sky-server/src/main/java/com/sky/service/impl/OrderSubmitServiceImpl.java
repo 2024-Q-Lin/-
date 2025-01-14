11dfce62d6442cd4adee4675b6d289ded8fe0fd4
joe <2193426510@qq.com>
2025-01-14 18:29:14 +0800

    检查是否超出配送范围业务功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/OrderSubmitServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderSubmitServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -13,6 +15,7 @@ import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.*;
 import com.sky.result.PageResult;
 import com.sky.service.OrderSubmitService;
+import com.sky.utils.HttpClientUtil;
 import com.sky.utils.WeChatPayUtil;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
@@ -22,12 +25,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -46,6 +52,10 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
     private WeChatPayUtil weChatPayUtil;
     @Autowired
     private UserMapper userMapper;
+    @Value("${sky.shop.address}")
+    private String shopAddress;
+    @Value("${sky.baidu.ak}")
+    private String ak;
 
     /**
      * 提交订单
@@ -61,6 +71,8 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         if (addressBook == null) {
             throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
         }
+        //超过配送范围
+        checkOutOfRange(addressBook.getCityName() + addressBook.getDistrictName() + addressBook.getDetail());
         //购物车为空
         ShoppingCart cart = new ShoppingCart();
         cart.setUserId(BaseContext.getCurrentId());
@@ -98,12 +110,7 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         shoppingCartMapper.deleteByUserId(BaseContext.getCurrentId());
 
         //查询返回值的内容
-        return OrderSubmitVO.builder()
-                .id(orders.getId())
-                .orderNumber(orders.getNumber())
-                .orderAmount(orders.getAmount())
-                .orderTime(orders.getOrderTime())
-                .build();
+        return OrderSubmitVO.builder().id(orders.getId()).orderNumber(orders.getNumber()).orderAmount(orders.getAmount()).orderTime(orders.getOrderTime()).build();
     }
 
     /**
@@ -118,8 +125,7 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         User user = userMapper.getById(userId);
 
         //调用微信支付接口，生成预支付交易单
-        JSONObject jsonObject = weChatPayUtil.pay(
-                ordersPaymentDTO.getOrderNumber(), //商户订单号
+        JSONObject jsonObject = weChatPayUtil.pay(ordersPaymentDTO.getOrderNumber(), //商户订单号
                 new BigDecimal(0.01), //支付金额，单位 元
                 "苍穹外卖订单", //商品描述
                 user.getOpenid() //微信用户的openid
@@ -146,12 +152,7 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         Orders ordersDB = orderMapper.getByNumber(outTradeNo);
 
         // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
-        Orders orders = Orders.builder()
-                .id(ordersDB.getId())
-                .status(Orders.TO_BE_CONFIRMED)
-                .payStatus(Orders.PAID)
-                .checkoutTime(LocalDateTime.now())
-                .build();
+        Orders orders = Orders.builder().id(ordersDB.getId()).status(Orders.TO_BE_CONFIRMED).payStatus(Orders.PAID).checkoutTime(LocalDateTime.now()).build();
 
         orderMapper.update(orders);
     }
@@ -240,8 +241,7 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         // 如果订单处于待接单状态下取消，需要进行退款
         if (ordersDB.getStatus().equals(OrderVO.TO_BE_CONFIRMED)) {
             //调用微信支付退款接口
-            weChatPayUtil.refund(
-                    ordersDB.getNumber(),//订单号
+            weChatPayUtil.refund(ordersDB.getNumber(),//订单号
                     ordersDB.getNumber(),//退款订单号
                     new BigDecimal(0.01),//退款金额，单位 元
                     new BigDecimal(0.01));//原订单金额
@@ -359,18 +359,20 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
 
     /**
      * 接单
+     *
      * @param ordersConfirmDTO
      */
     @Override
     public void confirm(OrdersConfirmDTO ordersConfirmDTO) {
         Orders orders = new Orders();
-        BeanUtils.copyProperties(ordersConfirmDTO,orders);
+        BeanUtils.copyProperties(ordersConfirmDTO, orders);
         orders.setStatus(Orders.CONFIRMED);
         orderMapper.update(orders);
     }
 
     /**
      * 拒单
+     *
      * @param ordersRejectionDTO
      */
     @Override
@@ -384,26 +386,21 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         //支付状态,如果已经支付即退款
         Integer payStatus = ordersDB.getPayStatus();
         if (payStatus.equals(Orders.PAID)) {
-            String refund = weChatPayUtil.refund(
-                    ordersDB.getNumber(),//订单号
+            String refund = weChatPayUtil.refund(ordersDB.getNumber(),//订单号
                     ordersDB.getNumber(),//退款订单号
                     new BigDecimal(0.01),//退款金额，单位 元
                     new BigDecimal(0.01));
-            log.info("申请退款：{}",refund);
+            log.info("申请退款：{}", refund);
         }
         //根据订单id更新订单状态、拒单原因、取消时间
-        Orders orders = Orders.builder()
-                .id(ordersRejectionDTO.getId())
-                .status(Orders.CANCELLED)
-                .rejectionReason(ordersRejectionDTO.getRejectionReason())
-                .cancelTime(LocalDateTime.now())
-                .build();
+        Orders orders = Orders.builder().id(ordersRejectionDTO.getId()).status(Orders.CANCELLED).rejectionReason(ordersRejectionDTO.getRejectionReason()).cancelTime(LocalDateTime.now()).build();
 
         orderMapper.update(orders);
     }
 
     /**
      * 管理端取消订单
+     *
      * @param ordersCancelDTO
      */
     @Override
@@ -412,26 +409,21 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         Orders ordersDB = orderMapper.getById(ordersCancelDTO.getId());
         // 如果订单已经支付，则退款
         if (ordersDB != null && ordersDB.getPayStatus().equals(Orders.PAID)) {
-            String refund = weChatPayUtil.refund(
-                    ordersDB.getNumber(),//订单号
+            String refund = weChatPayUtil.refund(ordersDB.getNumber(),//订单号
                     ordersDB.getNumber(),//退款订单号
                     new BigDecimal(0.01),//退款金额，单位 元
                     new BigDecimal(0.01));
-            log.info("申请退款：{}",refund);
+            log.info("申请退款：{}", refund);
         }
         //根据订单id更新订单状态、取消原因、取消时间
-        Orders orders = Orders.builder()
-                .id(ordersCancelDTO.getId())
-                .status(Orders.CANCELLED)
-                .cancelReason(ordersCancelDTO.getCancelReason())
-                .cancelTime(LocalDateTime.now())
-                .build();
+        Orders orders = Orders.builder().id(ordersCancelDTO.getId()).status(Orders.CANCELLED).cancelReason(ordersCancelDTO.getCancelReason()).cancelTime(LocalDateTime.now()).build();
 
         orderMapper.update(orders);
     }
 
     /**
      * 派送订单
+     *
      * @param id
      */
     @Override
@@ -441,15 +433,13 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
         if (ordersDB == null || !ordersDB.getStatus().equals(Orders.CONFIRMED)) {
             throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
         }
-        Orders orders = Orders.builder()
-                .id(id)
-                .status(Orders.DELIVERY_IN_PROGRESS)
-                .build();
+        Orders orders = Orders.builder().id(id).status(Orders.DELIVERY_IN_PROGRESS).build();
         orderMapper.update(orders);
     }
 
     /**
      * 完成订单
+     *
      * @param id
      */
     @Override
@@ -503,5 +493,70 @@ public class OrderSubmitServiceImpl implements OrderSubmitService {
 
         // 将订单菜品信息拼接在一起
         return String.join("", orderDDishList);
+    }
+
+    /**
+     * 检查客户的收货地址是否超出配送范围
+     *
+     * @param address
+     */
+    private void checkOutOfRange(String address) {
+        Map map = new HashMap();
+        map.put("address", shopAddress);
+        map.put("output", "json");
+        map.put("ak", ak);
+
+        //获取店铺的经纬度坐标
+        String shopCoordinate = HttpClientUtil.doGet("https://api.map.baidu.com/geocoding/v3", map);
+
+        JSONObject jsonObject = JSON.parseObject(shopCoordinate);
+        if (!jsonObject.getString("status").equals("0")) {
+            throw new OrderBusinessException("店铺地址解析失败");
+        }
+
+        //数据解析
+        JSONObject location = jsonObject.getJSONObject("result").getJSONObject("location");
+        String lat = location.getString("lat");
+        String lng = location.getString("lng");
+        //店铺经纬度坐标
+        String shopLngLat = lat + "," + lng;
+
+        map.put("address", address);
+        //获取用户收货地址的经纬度坐标
+        String userCoordinate = HttpClientUtil.doGet("https://api.map.baidu.com/geocoding/v3", map);
+
+        jsonObject = JSON.parseObject(userCoordinate);
+        if (!jsonObject.getString("status").equals("0")) {
+            throw new OrderBusinessException("收货地址解析失败");
+        }
+
+        //数据解析
+        location = jsonObject.getJSONObject("result").getJSONObject("location");
+        lat = location.getString("lat");
+        lng = location.getString("lng");
+        //用户收货地址经纬度坐标
+        String userLngLat = lat + "," + lng;
+
+        map.put("origin", shopLngLat);
+        map.put("destination", userLngLat);
+        map.put("steps_info", "0");
+
+        //路线规划
+        String json = HttpClientUtil.doGet("https://api.map.baidu.com/directionlite/v1/driving", map);
+
+        jsonObject = JSON.parseObject(json);
+        if (!jsonObject.getString("status").equals("0")) {
+            throw new OrderBusinessException("配送路线规划失败");
+        }
+
+        //数据解析
+        JSONObject result = jsonObject.getJSONObject("result");
+        JSONArray jsonArray = (JSONArray) result.get("routes");
+        Integer distance = (Integer) ((JSONObject) jsonArray.get(0)).get("distance");
+
+        if (distance > 5000) {
+            //配送距离超过5000米
+            throw new OrderBusinessException("超出配送范围");
+        }
     }
 }
